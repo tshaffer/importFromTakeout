107014bae88df165d59b6cd56616d34550d9e222
Ted Shaffer <ted@brightsign.biz>
2021-02-13 05:57:54 -0800

    add use of dotenv, mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-function main() {
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+export let mediaItemsDir: string = '';
+
+async function main() {
   console.log('main invoked');
+
+  dotenv.config({ path: './/src/config/config.env' });
+
+  // connect to db
+  await connectDB();
+
 }
 
 main();
---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+async function connectDB() {
+  console.log('uri is:');
+  console.log(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  mongoose.Promise = global.Promise;
+
+  console.log(`MongoDB Connected`);
+};
+
+export default connectDB;
