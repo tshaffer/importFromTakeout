e419e14d2a8f9f681828631a647fff9d8b2d4f34
Ted Shaffer <ted@brightsign.biz>
2021-02-14 06:04:26 -0800

    tried package ts-exif-parser.

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -1,24 +1,34 @@
-import { getImageFilesInDirectory } from './fileUtilities';
+import {
+  getFileBuffer,
+  getImageFilesInDirectory,
+} from './fileUtilities';
 
-var ExifImage = require('exif').ExifImage;
+import { ExifParserFactory } from "ts-exif-parser";
+
+// var ExifImage = require('exif').ExifImage;
 
 import { mediaItemsDir } from '../app';
 
 
-export const runApp = () => {
+export const runApp = async () => {
   const imageFiles = getImageFilesInDirectory(mediaItemsDir);
   console.log(imageFiles);
 
   const filePath = '/Volumes/SHAFFEROTO/takeout/unzipped/Takeout 15/Google Photos/Photos from 2020/IMG_4464.JPG';
 
-  try {
-    new ExifImage({ image: filePath }, function (error: any, exifData: any) {
-      if (error)
-        console.log('Error: ' + error.message);
-      else
-        console.log(exifData); // Do something with your data!
-    });
-  } catch (error) {
-    console.log('Error: ' + error.message);
-  }
+  // try {
+  //   new ExifImage({ image: filePath }, function (error: any, exifData: any) {
+  //     if (error)
+  //       console.log('Error: ' + error.message);
+  //     else
+  //       console.log(exifData); // Do something with your data!
+  //   });
+  // } catch (error) {
+  //   console.log('Error: ' + error.message);
+  // }
+
+  const data: Buffer = await getFileBuffer(filePath);
+  // const parser = ExifParserFactory.create(data);
+  const Data = ExifParserFactory.create(data).parse();
+  console.log(Data);
 }
--- src/controllers/fileUtilities.ts
+++ src/controllers/fileUtilities.ts
@@ -1,10 +1,11 @@
+import * as fs from 'fs';
 import path from 'path';
 import * as nodeDir from 'node-dir';
 
 const imageFileExtensions = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG'];
 
 const getFilesInDirectory = (rootDirPath: string) => {
-  return nodeDir.files(rootDirPath, {sync:true});
+  return nodeDir.files(rootDirPath, { sync: true });
 }
 
 export const getImageFilesInDirectory = (rootPath: string) => {
@@ -18,4 +19,27 @@ export const getImageFilesInDirectory = (rootPath: string) => {
     }
   }
   return imageFiles;
+}
+
+export const getFileBuffer = (filePath: string): Promise<Buffer> => {
+
+  const buffer: Buffer = Buffer.alloc(65635);
+
+  const fd: any = fs.openSync(filePath, 'r');
+  return new Promise((resolve, reject) => {
+    fs.read(fd, buffer, 0, buffer.length, 0, (err, bytes) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      } else {
+        fs.close(fd, function (err) {
+          if (err) {
+            console.log(err);
+            return reject(err);
+          }
+        });
+        return resolve(buffer);
+      }
+    })
+  })
 }
