b83fab4dd9ada180895cd47f7b53af8b530ff881
Ted Shaffer <ted@brightsign.biz>
2021-02-14 05:31:04 -0800

    initial use of node-exif

--- src/controllers/appController.ts
+++ src/controllers/appController.ts
@@ -1,8 +1,24 @@
 import { getImageFilesInDirectory } from './fileUtilities';
 
+var ExifImage = require('exif').ExifImage;
+
 import { mediaItemsDir } from '../app';
+
 
 export const runApp = () => {
   const imageFiles = getImageFilesInDirectory(mediaItemsDir);
   console.log(imageFiles);
+
+  const filePath = '/Volumes/SHAFFEROTO/takeout/unzipped/Takeout 15/Google Photos/Photos from 2020/IMG_4464.JPG';
+
+  try {
+    new ExifImage({ image: filePath }, function (error: any, exifData: any) {
+      if (error)
+        console.log('Error: ' + error.message);
+      else
+        console.log(exifData); // Do something with your data!
+    });
+  } catch (error) {
+    console.log('Error: ' + error.message);
+  }
 }
