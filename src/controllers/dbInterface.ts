d15598f0a3c1988d32a08b7be68e3cee0dd08996
Ted Shaffer <ted@brightsign.biz>
2021-02-20 05:40:38 -0800

    replaced any with a type

--- src/controllers/dbInterface.ts
+++ src/controllers/dbInterface.ts
@@ -1,5 +1,6 @@
 import { DbMediaItem } from "../types";
 import Mediaitem from '../models/Mediaitem';
+import { Query } from "mongoose";
 
 export const addMediaItemToDb = async (dbMediaItem: DbMediaItem): Promise<string> => {
   return Mediaitem.collection.insertOne(dbMediaItem)
@@ -9,7 +10,7 @@ export const addMediaItemToDb = async (dbMediaItem: DbMediaItem): Promise<string
     })
 };
 
-export const setMediaItemFilePathInDb = async (id: string, filePath: string): Promise<any> => {
+export const setMediaItemFilePathInDb = async (id: string, filePath: string): Promise<Query<any, any>> => {
   return Mediaitem.findByIdAndUpdate(id, { filePath });
 };
 
